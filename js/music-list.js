00d29a610b14ca338b588fb4c38359541471fff7
khosrowshahi <52390204+khosrowshahi@users.noreply.github.com>
2021-07-25 21:58:59 +0430

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+  {
+    name: "Jange Delam",
+    artist: "Shadmehr",
+    img: "jangeDelam",
+    src: "jangeDelam"
+
+  },
+  {
+    name: "Not Worth It",
+    artist: "Hamed Behdad",
+    img: "notWorthIt",
+    src: "notWorthIt"
+  },
+  {
+    name: "Rage Khaab",
+    artist: "Homayoun Shajarian",
+    img: "rageKhaab",
+    src: "rageKhaab"
+  },
+  {
+    name: "Sefareshi",
+    artist: "YAS",
+    img: "sefareshi",
+    src: "sefareshi"
+  },
+];
